01b95acd079016ac7ef8425af1e371e6513b2faf
anlaganlag <tanjhu@163.com>
2025-03-13 23:58:51 +0800

    v1

--- config.py
+++ config.py
@@ -2,9 +2,13 @@
 
 # Columns to preserve from the input file (green headers)
 PRESERVED_COLUMNS = [
+    'NO.',
     'DESCRIPTION',
+    'Model NO.',
     'Qty',
-    'Amount'
+    'Unit',
+    'Unit Price(USD)',
+    'Amount(USD)'
     # Add more columns as needed
 ]
 
@@ -14,17 +18,21 @@ MATERIAL_CODE_COLUMN = 'Material code'
 # Columns to match from the reference file (yellow headers)
 MATCHED_COLUMNS = [
     '商品编号',
+    '申报要素',
     # Add more columns as needed
 ]
 
 # Fixed columns to add to the output file
 FIXED_COLUMNS = {
-    '币值1': '美元',
-    '币值2': '人民币',
-    '币值3': 'BTC',
+    '币制': '美元',
+    '原产国（地区）': '中国',
+    '最终目的国（地区）': '印度',
+    '境内货源地': '深圳特区',
+    '征免': '照章征税'
+
 
     # Add more columns as needed
 }
 
 # Output file name
-OUTPUT_FILE_NAME = 'output.xlsx'    
+OUTPUT_FILE_NAME = 'output.xlsx'
